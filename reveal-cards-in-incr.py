856a816ee8afa0862531b8efeb89c076b8d31e55
Shankar, K <kshan_77@yahoo.com>
2018-12-09 14:33:15 -0800

    this week's lc - i

--- reveal-cards-in-incr.py
+++ reveal-cards-in-incr.py
@@ -1,7 +1,22 @@
 class Solution(object):
-    def deckRevealedIncreasing(self, deck):
+    def deckRevealedIncreasing(self, d):
         """
         :type deck: List[int]
         :rtype: List[int]
         """
-        
+        n = len(d)
+        d.sort()
+        il, c, i = range(n),0,0
+        ans = [0]*n
+        while c < n-1:
+            ans[il[i]] = d[c]
+            i += 2
+            c += 1
+            il.append(il[i-1])
+        ans[il[-1]] = d[c]
+        return ans
+print Solution().deckRevealedIncreasing([17])
+print Solution().deckRevealedIncreasing([17,12])
+print Solution().deckRevealedIncreasing([17,7,12])
+print Solution().deckRevealedIncreasing([17,13,11,2,3,5,7])
+print Solution().deckRevealedIncreasing([17,13,11,2,5,7])
