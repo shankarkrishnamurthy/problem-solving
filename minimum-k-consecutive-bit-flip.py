8fc0dfd4443ce87746bca5b0d7d0450e9888b59d
Shankar, K <kshan_77@yahoo.com>
2019-02-19 09:35:32 -0800

    left overs

--- minimum-k-consecutive-bit-flip.py
+++ minimum-k-consecutive-bit-flip.py
@@ -5,7 +5,6 @@ class Solution(object):
         :type K: int
         :rtype: int
         """
-        #print '\nA ', A
         F,n = [], len(A)
         for i in range(0,n-K+1):
             if i == 0:
@@ -17,12 +16,9 @@ class Solution(object):
                 F.append(1 + F[i-1]) # one more flip
             else:
                 F.append(F[i-1])
-            #print i, A[i], m, 'Flips ', F
-        #print 'Flips done ', F
         lf = F[-1]
         for i in range(n-K+1,n):
             m = F[i-1] - (F[i-K] if i >=K else 0)
-            #print i, A[i], m, 'Flips ', F
             if (m % 2 == 0 and A[i] == 0) or (m % 2 == 1 and A[i] == 1): return -1
             F.append(F[i-1])
             
