40c9fe0b9e464375caf8670d97a23a144ebeb9c6
k. shankar <kshan_77@yahoo.com>
2024-01-13 12:34:41 -0800

    sieve

---
+++ sieve-of-eratosthenes.py
@@ -0,0 +1,13 @@
+def walk(n):
+    prime=[i for i in range(n+1)]
+    prime[0],prime[1] = -1, -1
+    for i in range(2, n + 1):
+        if prime[i] == i:
+            for j in range(i * i, n + 1, i):
+                prime[j] = i
+        print(i, prime)
+    pr = [i for i in range(n+1) if i == prime[i]]
+    return (prime,pr)
+    
+sieve, pr = walk(30)
+print(len(pr), pr)
